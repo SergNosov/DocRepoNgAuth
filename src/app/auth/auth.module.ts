b9adbaad024e1dc82e8f7b8b441698ba7b940333
Serg N <serg_vn@inbox.ru>
2020-08-04 10:39:18 +0900

    create auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RegisterComponent} from './components/register/register.component';
+import {RouterModule} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
+
+const routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: RegisterComponent}
+];
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule
+  ]
+})
+export class AuthModule {
+}
