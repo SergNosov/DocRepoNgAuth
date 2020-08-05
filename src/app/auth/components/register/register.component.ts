574ac6693d30247923d09da6a2e6cb0e148255ac
Serg N <serg_vn@inbox.ru>
2020-08-05 15:06:06 +0900

    adding auth.service.ts

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Store} from '@ngrx/store';
+import {select, Store} from '@ngrx/store';
 import {registerAction} from '../../store/actions/register.action';
+import {Observable} from 'rxjs';
+import {isSubmittingSelector} from '../../store/selectors';
 
 @Component({
   selector: 'app-register',
@@ -11,12 +13,14 @@ import {registerAction} from '../../store/actions/register.action';
 export class RegisterComponent implements OnInit {
 
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
 
   constructor(private fb: FormBuilder, private store: Store) {
   }
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeValues();
   }
 
   initializeForm(): void {
@@ -24,6 +28,11 @@ export class RegisterComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', [Validators.minLength(3), Validators.required]]
     });
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+    console.log('isSubmiting$', this.isSubmitting$);
   }
 
   onSubmit(): void {
